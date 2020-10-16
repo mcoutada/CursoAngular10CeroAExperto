b0d3d7b4f3497c568210b6815e970a2010e4ef62
Mariano <mcoutada@gmail.com>
2020-10-16 10:19:51 -0300

    ...

--- Angular/02-spa/src/app/app.module.ts
+++ Angular/02-spa/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+ 
+ 
 // Rutas
 import { APP_ROUTING } from './app.routes';
-
-
+ 
+ 
 // Servicios
-
+ 
  
 // Componentes
 import { AppComponent } from './app.component';
@@ -15,7 +15,7 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
