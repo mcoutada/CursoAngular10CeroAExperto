050a00a5867afb4324c9ddad1b683033cb42aa30
Mariano <mcoutada@gmail.com>
2020-12-02 22:29:38 -0300

    68

--- Angular/02-spa/src/app/app.module.ts
+++ Angular/02-spa/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { FormsModule } from '@angular/forms';
 import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { BuscadorComponent } from './components/buscador/buscador.component';
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
